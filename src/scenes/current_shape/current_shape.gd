ea3b71688a7f529dc9e9bca3b6480e5376b3afd4
Alexey Korepanov <alexkorep@gmail.com>
2024-01-13 09:55:24 +0100

    Add dragging and rotation

--- src/scenes/current_shape/current_shape.gd
+++ src/scenes/current_shape/current_shape.gd
@@ -1,10 +1,13 @@
 extends TileMap
 
+var mouse_down = false
 var dragging = false
 var drag_start = Vector2()
 var original_position = Vector2()
 
 var tween = Tween.new()
+
+var DRAG_DISTANCE_THRESHOLD = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,21 +16,28 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		if not dragging:
-			dragging = true
+		if not mouse_down:
+			mouse_down = true
 			drag_start = get_global_mouse_position()
 			original_position = position
 	else:
-		if dragging:
-			dragging = false
-			go_back()
-			
+		if mouse_down:
+			mouse_down = false
+			if dragging:
+				dragging = false
+				go_back()
+			else:
+				# Rotate the TileMap by 90 degrees
+				rotation_degrees += 90
+
+	var distance_between_origin_and_mouse = (get_global_mouse_position() - drag_start).length()
+	if distance_between_origin_and_mouse > DRAG_DISTANCE_THRESHOLD and mouse_down:
+		dragging = true
 
 	if dragging:
 		var drag_current = get_global_mouse_position()
 		var drag_delta = drag_current - drag_start
-		position += drag_delta
-		drag_start = drag_current
+		position = original_position + drag_delta
 
 func go_back():
 	# Use tween to animate the tilemap back to the original position
